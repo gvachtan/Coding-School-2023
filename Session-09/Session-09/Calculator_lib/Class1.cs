bdd11ef7f90ed1e825513045f41acb8c23d14a63
Giorgos Vachtanidis <vachtasgr@gmail.com>
2023-01-19 16:48:38 +0200

    9

---
+++ Session-09/Session-09/Calculator_lib/Class1.cs
@@ -0,0 +1,35 @@
+﻿namespace Calculator_lib
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        public double RaiseToPower(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+
+        public double SquareRoot(double a)
+        {
+            return Math.Sqrt(a);
+        }
+    }
+}
---
+++ Session-09/Session-09/Session-09/Form1.cs
@@ -0,0 +1,15 @@
+namespace Session_09
+{
+    public partial class Form1 : Form  
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
